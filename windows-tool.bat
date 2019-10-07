4e72d3c1366523209ce297ad34f3bfb645d46be7
ViperZCrew <47422277+ViperZCrew@users.noreply.github.com>
2019-10-07 23:11:22 +0000

    Add files via upload

---
+++ windows-tool.bat
@@ -0,0 +1,13 @@
+title [MrBlackX] Downloading tools...
+echo off
+color 0d
+echo This could need little bit time...
+color 0b
+echo Starting downloader...
+color 0a
+echo Click enter to start!
+pause >NUL
+cd Downloads
+curl -LO https://srv-file7.gofile.io/download/JUj4aA/all_ddos_windows_mrblackx.zip
+echo Download successfull!
+pause >NUL
